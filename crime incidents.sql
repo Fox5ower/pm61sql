f8f21eb8e8d28e2543dd9ecf59407a52f26925ca
Lisikov Maxim <43184143+Fox5ower@users.noreply.github.com>
2022-01-19 12:07:16 +0100

    Create crime incidents.sql

---
+++ crime incidents.sql
@@ -0,0 +1,10 @@
+WITH crime_and_region AS
+  (SELECT "mysql"."promethium"."US_REGIONS"."US_STATE" AS "State Code",
+          "mysql"."promethium"."COMBINED_CRIME_INCIDENTS"."COMBINED_CRIME_INCIDENTS" AS "COMBINED_CRIME_INCIDENTS"
+   FROM "mysql"."promethium"."COMBINED_CRIME_INCIDENTS"
+   LEFT OUTER JOIN "mysql"."promethium"."US_REGIONS" ON ("mysql"."promethium"."COMBINED_CRIME_INCIDENTS"."REGION_CODE" = "mysql"."promethium"."US_REGIONS"."REGION_CODE"))
+SELECT "State Code",
+       SUM(COMBINED_CRIME_INCIDENTS) as "Total Crimes"
+FROM crime_and_region
+GROUP BY "State Code"
+LIMIT 100
